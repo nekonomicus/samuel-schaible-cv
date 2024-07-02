49bc3166f85c57d9ac40178de85a7cca8b8cc73b
nekonomicus <87386132+nekonomicus@users.noreply.github.com>
2024-07-02 10:49:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,14 @@ backToTopButton.addEventListener('click', scrollToTop);
 document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const headerOffset = 60; // Adjust this value based on your header height
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
             behavior: 'smooth'
         });
     });
